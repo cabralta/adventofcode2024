90a37405024f08a5ce710ea4af75d85b46c5b337
Tomas Cabral <tomas.cabral@gmail.com>
2024-12-31 03:52:51 -0500

    init day4

--- day/4/day4.go
+++ day/4/day4.go
@@ -1,1 +1,110 @@
 package main
+
+import (
+	"log/slog"
+	"time"
+
+	"adventofcode2024.com/day/utils"
+)
+
+func CalculateMatches(Input []string, Match string) int {
+
+	SubTotal := 0
+
+	for _, Str := range Input {
+		Match := utils.RexString(Str, Match)
+		SubTotal += len(Match)
+	}
+
+	return SubTotal
+}
+
+func CheckForPattern(Matrix [][]string, x int, y int, MatchTerm string) int {
+	//Check East
+
+	var EastString string
+	var WestString string
+	var SouthString string
+	var NorthString string
+	var NorthEastString string
+	var NorthWestString string
+	var SouthEastString string
+	var SouthWestString string
+	Matches := 0
+	MatchLen := len(MatchTerm)
+	MatchMax := MatchLen - 1
+	//create strings to match with starting vector
+
+	for i := 0; i < MatchLen; i++ {
+
+		if (y + MatchMax) < len(Matrix[0]) {
+			EastString += string(Matrix[x][y+i])
+		}
+
+		if (y - MatchMax) >= 0 {
+			WestString += string(Matrix[x][y-i])
+		}
+
+		if (x + MatchMax) < len(Matrix) {
+			SouthString += string(Matrix[x+i][y])
+
+			if (y + MatchMax) < len(Matrix[0]) {
+				SouthEastString += string(Matrix[x+i][y+i])
+			}
+
+			if (y - MatchMax) >= 0 {
+				SouthWestString += string(Matrix[x+i][y-i])
+			}
+		}
+
+		if (x - MatchMax) >= 0 {
+			NorthString += string(Matrix[x-i][y])
+
+			if (y + MatchMax) < len(Matrix[0]) {
+				NorthEastString += string(Matrix[x-i][y+i])
+			}
+
+			if (y - MatchMax) >= 0 {
+				NorthWestString += string(Matrix[x-i][y-i])
+			}
+
+		}
+
+	}
+
+	DirectionalStrings := []string{EastString, WestString, NorthString, SouthString, NorthEastString, NorthWestString, SouthEastString, SouthWestString}
+
+	for _, Str := range DirectionalStrings {
+
+		if Str == MatchTerm {
+			Matches++
+		}
+	}
+
+	return Matches
+}
+
+func main() {
+
+	Total := 0
+	//path := "testdata/test_input"
+	path := "data/aoc_data"
+	defer utils.TimeTrack(time.Now(), "Day 4")
+
+	StringMatrix := utils.ReadFileIntoStringSlice(path)
+	MatchStr := "XMAS"
+	//MatchTerm := []byte(MatchStr)
+
+	for x, line := range StringMatrix {
+		for y, _ := range line {
+			//fmt.Printf("%s ", string(char))
+			Total += CheckForPattern(StringMatrix, x, y, MatchStr)
+
+		}
+
+		//fmt.Printf("\n")
+
+	}
+	slog.Info("Match Calculation Completed Successfully.", slog.Int("Total", Total))
+
+}
--- day/utils/utils.go
+++ day/utils/utils.go
@@ -4,28 +4,19 @@ import (
 	"bufio"
 	"log/slog"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
+// Calculations
 func Abs(Integer int) int {
 	if Integer < 0 {
 		return -Integer
 	}
 
 	return Integer
-}
-
-func StringsToInts(Input []string) []int {
-
-	Ints := make([]int, 0)
-	for _, StringToConvert := range Input {
-		Int, _ := strconv.Atoi(StringToConvert)
-		Ints = append(Ints, Int)
-	}
-
-	return Ints
 }
 
 func GetProductOfInts(IntArr []int) int {
@@ -43,6 +34,120 @@ func GetProductOfInts(IntArr []int) int {
 	}
 
 	return Product
+}
+
+// Slice Functions
+
+func StringsToInts(Input []string) []int {
+
+	Ints := make([]int, 0)
+	for _, StringToConvert := range Input {
+		Int, _ := strconv.Atoi(StringToConvert)
+		Ints = append(Ints, Int)
+	}
+
+	return Ints
+}
+
+func RemoveItemFromSlice(Slice []int, S int) []int {
+	//We don't want to modify the existing slice but provide a new slice with the missing value
+	NewSlice := make([]int, 0)
+	NewSlice = append(NewSlice, Slice[:S]...)
+	return append(NewSlice, Slice[S+1:]...)
+}
+
+func GetReverseStringSlice(Input []string) []string {
+	ReverseSlice := make([]string, 0)
+
+	for _, Str := range Input {
+		ReverseSlice = append(ReverseSlice, ReverseString(Str))
+	}
+
+	return ReverseSlice
+}
+
+func GetVerticalStringSlice(Input []string) []string {
+
+	VerticalSlice := make([]string, len(Input))
+
+	for _, s := range Input {
+		for j, AsciiRune := range s {
+			str := string(AsciiRune)
+			VerticalSlice[j] += str
+		}
+	}
+
+	return VerticalSlice
+}
+
+func GetDiagonalStringSlice(Input []string) []string {
+
+	DiagonalSlice := make([]string, ((2 * len(Input)) - 1))
+	//MaxH := len(Input)-1
+	MaxL := len(Input[0]) - 1
+
+	//CalculateVectorDifference
+	//MaxL + VectorDiff = DiagonalSlice[i]+=CurrentVector
+
+	for i, line := range Input {
+
+		for j, char := range line {
+			VectorDiff := i - j
+			DiagonalSliceIdx := MaxL - VectorDiff
+			DiagonalSlice[DiagonalSliceIdx] += string(char)
+		}
+	}
+
+	return DiagonalSlice
+}
+
+// String Functions
+func RexString(Input string, Rex string) []string {
+
+	rex, _ := regexp.Compile(Rex)
+	return rex.FindAllString(Input, -1)
+
+}
+
+func ReverseString(Input string) string {
+
+	ReverseString := make([]byte, len(Input))
+	StrLen := len(Input) - 1
+
+	for i, value := range Input {
+		ReverseString[StrLen-i] = byte(value)
+	}
+
+	return string(ReverseString)
+
+}
+
+func ConvertStringToStringSlice(Input string) []string {
+	StringSlice := make([]string, 0)
+
+	for _, char := range Input {
+		StringSlice = append(StringSlice, string(char))
+	}
+	return StringSlice
+}
+
+// File Operations
+func ReadFileIntoStringSlice(Path string) [][]string {
+	FileContents := make([][]string, 0)
+	file, err := os.Open(Path)
+	if err != nil {
+		slog.Error("Error opening file.", Path, slog.Any("error", err))
+		panic(err)
+	}
+	defer file.Close()
+	Scanner := bufio.NewScanner(file)
+	for Scanner.Scan() {
+		line := Scanner.Text()
+		FileContents = append(FileContents, ConvertStringToStringSlice(line))
+
+	}
+
+	return FileContents
 }
 
 func ReadFileIntoString(Path string) []string {
@@ -94,12 +199,7 @@ func ReadFileIntoIntMatrix(Path string) [][]int {
 	return IntMatrix
 }
 
-func RemoveItemFromSlice(Slice []int, S int) []int {
-	//We don't want to modify the existing slice but provide a new slice with the missing value
-	NewSlice := make([]int, 0)
-	NewSlice = append(NewSlice, Slice[:S]...)
-	return append(NewSlice, Slice[S+1:]...)
-}
+// General
 
 func TimeTrack(Start time.Time, MethodName string) time.Duration {
 	Elapsed := time.Since(Start)
